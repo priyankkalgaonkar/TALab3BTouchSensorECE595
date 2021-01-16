2b4d1df4b23116ee71aabbda4021cd7047b4eaee
priyank12p <null>
2021-01-16 00:15:29 +0000

    Initial Commit

--- main.cpp
+++ main.cpp
@@ -1,20 +1,13 @@
 #include "mbed.h"
+DigitalIn button(D2);
 
-DigitalOut buzzer(D2);
-DigitalOut led_green(LED_GREEN);
-InterruptIn sw2(SW2);
-
-void sw2_release(void)
-{
-    led_green = !led_green;
-    buzzer = !buzzer;
-    
-    printf("On-board button SW2 was released.\n");
-}
-
-int main()
-{
-    sw2.rise(&sw2_release);
-    while (true) {
+int main (void) { 
+    int cnt = 0;
+    while(1) {
+        if (button) {
+            cnt++;
+            printf("Touch detected %d\n", cnt);
+            while(button);
+        }
     }
 }
